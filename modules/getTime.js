6bcaece5aede8fb0772acbaee03398f493eee7ef
cosmosha <cosmoshagan@gmail.com>
2023-02-21 14:03:29 +0200

    Update getTime.js

--- modules/getTime.js
+++ modules/getTime.js
@@ -2,6 +2,6 @@ import { DateTime } from './luxon.js';
 
 const getTime = document.querySelector('.time');
 
-getTime.innerHTML = DateTime.now();
+getTime.innerHTML = DateTime.now().toLocaleString(DateTime.DATETIME_FULL_WITH_SECONDS);
 
 export default getTime;
