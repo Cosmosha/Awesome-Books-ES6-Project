df94eb261489ce78c37c1bb1a44dde320a6ccf81
cosmosha <cosmoshagan@gmail.com>
2023-02-21 13:55:43 +0200

    Add luxon.js, + Update getTime.js

--- modules/getTime.js
+++ modules/getTime.js
@@ -1,5 +1,4 @@
-// eslint-disable-next-line import/no-unresolved
-import { DateTime } from '../../../../../node_modules/luxon/build/es6/luxon.js';
+import { DateTime } from './luxon.js';
 
 const getTime = document.querySelector('.time');
 
